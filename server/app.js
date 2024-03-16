c2d15c5a97386abff0c05a4fb92401fdf64c7b57
avikuuuu <ku.kumar.avdesh786@gmail.com>
2024-03-16 23:57:36 +0530

    add config and vite

--- server/app.js
+++ server/app.js
@@ -1,20 +1,37 @@
-import express from 'express'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
+// CONFIGRATIONS
 
-const app=express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("assets", express.static(path.join(__dirname, "public/assets")));
 
+// FILE STORAGE
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename:function(req,file,cb){
+    cb(null,file.originalname)
+  }
+});
 
+const upload=  multer({storage})
 
-
-
-
-
-
-
-
-
-
-
-
-export {app}
+export { app };
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,8 @@
 import  mongoose  from "mongoose";
 
-
-
 export const ConnectDB=async()=>{
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("db connected");
         
     } catch (error) {
