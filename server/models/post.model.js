4326d281dd07505807af1cbd6579b389b141ad66
avikuuuu <ku.kumar.avdesh786@gmail.com>
2024-03-17 23:00:08 +0530

    some fixes

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose"); // Erase if already required
+import mongoose from "mongoose"; // Erase if already required
 
 // Declare the Schema of the Mongo model
 var postSchema = new mongoose.Schema(
