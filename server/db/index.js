63fa758865cae7b16cdffe30a3def66c5df3a079
avikuuuu <ku.kumar.avdesh786@gmail.com>
2024-03-17 23:32:16 +0530

    add Backend Dummy data

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
 
 export const ConnectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}` );
+    const mongooseString=`${process.env.MONGO_URI}/${DB_NAME}`
+    // console.log('====================================');
+    // console.log(mongooseString);
+    // console.log('====================================');
+    const connectionInstance = await mongoose.connect(mongooseString );
     console.log(`db connected Host!!  ${connectionInstance.connection.host} `);
   } catch (error) {
     console.log("Error while connecting to database : " + error);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import { app } from "./app.js";
 import { ConnectDB } from "./db/index.js";
 import dotenv from "dotenv";
+import { User } from "./models/user.model.js";
+import { Post } from "./models/post.model.js";
+import {users,posts} from './data/index.js'
 
 dotenv.config({
   path: "./.env",
@@ -13,5 +16,8 @@ ConnectDB()
     app.listen(port, () => {
       console.log(`Server is running on port: ${"http://localhost:" + port}`);
     });
-  })
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  }
+  )
   .catch((err) => console.log("Error", err));
