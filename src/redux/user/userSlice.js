27caa9cf96db4ce9d2fb26d379d37f443bb5eb50
avikuuuu <ku.kumar.avdesh786@gmail.com>
2024-03-24 17:36:08 +0530

    done redux

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.Friends = action.payload.Friends;
+      } else {
+        console.error("user friends non-existent :(");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.Posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) =>
+        post._id === action.payload._id ? action.payload.post : post
+      );
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+
+export const { setMode, setLogin, logout, setFriends, setPosts, setPost } = userSlice.actions;
+export default userSlice.reducer;
