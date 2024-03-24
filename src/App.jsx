4cc5c45b55ee1369b4c588cc5726b4ef5f4fd1b8
avikuuuu <ku.kumar.avdesh786@gmail.com>
2024-03-24 16:23:19 +0530

    frontend project structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="app">
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+
   )
 }
 
